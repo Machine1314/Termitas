5827724661910ec26f3f8e3a7811851712c8b90d
Bruno <bruno.duenas@udla.edu.ec>
2022-05-01 16:19:23 -0500

    EXAMEN P1

--- termite.py
+++ termite.py
@@ -1,6 +1,39 @@
+# Metodo para sacar la llave del diccionario de poschips mediante su valor
+def get_key(val, posChips):
+    for key, value in posChips.items():
+        if val == value:
+            return key
+
+
+# Metodo que devuelve la distancia euclidiana de una coordenada comparada con la coordenada de la clave 0 del poschips
+def calcDist(position, posChips):
+    import numpy as np
+    # Pasamos las coordenadas a array para poder restarlos
+    posA = np.asarray(position)
+    posB = np.asarray(get_key(0, posChips))  # Obtenemos la coordenada del valor 0
+    return np.linalg.norm(posA - posB)  # retornamos al distancia euclidiana
+
+
+# Metodo que genera las distancias y diccionario de posibles movimientos
+def generateDistances(children, posChips):
+    distances = []
+    posibleMoves = {}
+    # Por cada hijo calculamos la distancia euclidiana
+    for child in children:
+        distances.append(calcDist(child, posChips))
+        # Agregamos al diccionario la clave (distancia euclidiana) y valor actual (coordenada)
+        posibleMoves[calcDist(child, posChips)] = child
+    return distances, posibleMoves
+
+
+# Retorna la menor distancia de un arreglo
+def getShortestDistance(distances):
+    return min(distances)
+
+
 class Termite:
 
-    def __init__(self, posicion=(0, 0), color="red"):
+    def __init__(self, posicion=(0, 0), color="green"):
         self.last_postion = posicion
         self.posicion = posicion
         self.color = color
@@ -12,76 +45,74 @@ class Termite:
     def getColor(self):
         return self.color
 
+    # Funciones de sucesor
     def moveUp(self, interval=1):
-        self.posicion = (self.posicion[0], self.posicion[1] + interval)
+        return self.posicion[0], (self.posicion[1] + interval)
 
     def moveDown(self, interval=1):
-        self.posicion = (self.posicion[0], self.posicion[1] - interval)
+        return self.posicion[0], (self.posicion[1] - interval)
 
     def moveRight(self, interval=1):
-        self.posicion = (self.posicion[0] + interval, self.posicion[1])
+        return (self.posicion[0] + interval), self.posicion[1]
 
     def moveLeft(self, interval=1):
-        self.posicion = (self.posicion[0] - interval, self.posicion[1])
+        return (self.posicion[0] - interval), self.posicion[1]
 
-    def move(self, r, limits, interval=1):
-        mov = r.randint(0, 3)
-        if mov == 0:
-            if self.posicion[1] < limits[1]:
-                self.moveUp(interval)
-        elif mov == 1:
-            if self.posicion[1] > limits[0]:
-                self.moveDown(interval)
-        elif mov == 2:
-            if self.posicion[0] > limits[2]:
-                self.moveLeft(interval)
-        elif mov == 3:
-            if self.posicion[0] < limits[3]:
-                self.moveRight(interval)
+    # Noreste
+    def moveNE(self, interval=1):
+        return self.posicion[0] + interval, self.posicion[1] + interval
 
-    def checkChip(self, Chips, posChips, rand, limits, interval, termite, tc):
-        if self.posicion in posChips and self.color == "red":
-            if Chips[posChips[self.posicion]].color != "white" and self.posicion != self.last_postion:
-                print('Toma una chip>> ', Chips[posChips[self.posicion]].color)
-                self.color = "green"
-                self.load = posChips[self.posicion]
-                Chips[self.load].color = "white"
-                self.last_postion = self.posicion
-                return self.posicion
-            else:
-                print('No coge la chip que recién dejo')
-        elif self.posicion in posChips and self.color == "green":
-            if Chips[posChips[self.posicion]].color == "blue":
-                print('Choca con chip sin tomar ')
-                self.move(rand, limits, interval)
-                print('*Se mueve*')
-                tc.color(termite.getColor())
-                tc.goto(termite.getPos())
-                if self.posicion in posChips:
-                    print('Se mueve a una chip')
-                    if Chips[posChips[self.posicion]].color == "white":
-                        print('Se mueve a una chip libre')
-                        print('Deja una chip>> ', Chips[posChips[self.posicion]].index)
-                        self.color = "red"
-                        print("Pos chips antes: ", Chips[self.load].posicion)
-                        Chips[self.load].posicion = self.posicion
-                        print("Pos chips despues: ", Chips[self.load].posicion)
-                        Chips[self.load].color = "blue"
-                        self.load = None
-                        self.last_postion = self.posicion
-                        return self.posicion
-                else:
-                    print('Deja una chip en un espacio en blanco ')
-                    self.color = "red"
-                    print("Pos chips antes: ", Chips[self.load].posicion)
-                    # print(posChips)
-                    posChips[self.getPos()] = posChips.pop(Chips[self.load].posicion)
-                    Chips[self.load].posicion = self.posicion
-                    print("Pos chips despues: ", Chips[self.load].posicion)
-                    Chips[self.load].color = "blue"
-                    self.load = None
-                    self.last_postion = self.posicion
-                    return self.posicion
+    # Sureste
+    def moveSE(self, interval=1):
+        return self.posicion[0] + interval, self.posicion[1] - interval
+
+    # Noroeste
+    def moveNW(self, interval=1):
+        return self.posicion[0] - interval, self.posicion[1] + interval
+
+    # Suroeste
+    def moveSW(self, interval=1):
+        return self.posicion[0] - interval, self.posicion[1] - interval
+
+    # Metodo que genera los nodos hijos
+    def generateChildren(self, limits, interval=1):
+        children = []
+        for mov in range(8):
+            if mov == 0:
+                # Se valida si esta dentro del rango del canvas
+                if self.posicion[1] < limits[1]:
+                    # Si cumple se agrega la coordenada al array
+                    children.append(self.moveUp(interval))
+            elif mov == 1:
+                if self.posicion[1] > limits[0]:
+                    children.append(self.moveDown(interval))
+            elif mov == 2:
+                if self.posicion[0] > limits[2]:
+                    children.append(self.moveLeft(interval))
+            elif mov == 3:
+                if self.posicion[0] < limits[3]:
+                    children.append(self.moveRight(interval))
+            elif mov == 4:
+                if self.posicion[0] < limits[3] and self.posicion[1] < limits[1]:
+                    children.append(self.moveNE(interval))
+            elif mov == 5:
+                if self.posicion[0] < limits[3] and self.posicion[1] > limits[0]:
+                    children.append(self.moveSE(interval))
+            elif mov == 6:
+                if self.posicion[0] > limits[2] and self.posicion[1] < limits[1]:
+                    children.append(self.moveNW(interval))
+            elif mov == 7:
+                if self.posicion[0] > limits[2] and self.posicion[1] > limits[0]:
+                    children.append(self.moveSW(interval))
+        return children
+
+    # Metodo que controla los proceos del termite
+    def checkMove(self, posChips, limits, interval, stepsWalk):
+        children = self.generateChildren(limits, interval)  # Generamos hijos
+        distances, posibleMoves = generateDistances(children, posChips)   # Generamos las distancia y posibles movimientos
+        minDistance = getShortestDistance(distances)  # Sacamos la menor distancia
+        self.posicion = posibleMoves[minDistance]  # Actualizamos nuestra posicion a las coordenadas con la menor distancia
+        stepsWalk.append(self.posicion)  # Agregamos la coordenada al array de pasos dados
 
 
 class Chip:
@@ -96,3 +127,10 @@ class Chip:
 
     def getColor(self):
         return self.color
+
+    # Metodo que mueve la chip a un lugar aleatorio
+    def moveChip(self, Chips, posChips, limits, r):
+        x, y = r.randint(limits[0], limits[1]), r.randint(limits[2], limits[3])  # Generamos x, y
+        posChips[(x, y)] = posChips.pop(self.posicion)  # Actualizamos el diccionario de la posicion de las chips con la nueva coordenada
+        Chips[posChips[(x, y)]].posicion = (x, y)  # Actualizamos la posicion de la chip
+        return x, y  # Retornamos la posicion para graficar
--- walkTermites.py
+++ walkTermites.py
@@ -1,22 +1,28 @@
-def walk(steps, interval, termites, chips, maxLimit):
+def walk(steps, interval, termites, chips, maxLimit, stepLimit):
     import turtle as t
     import random as r
     import termite as te
     from time import sleep
 
     t.setworldcoordinates(-maxLimit - 2, -maxLimit - 2, maxLimit + 2, maxLimit + 2)
+    t.title('EXAMEN P1 BRUNO DUEÑAS')
+    t.hideturtle()
     limits = [-maxLimit, maxLimit, -maxLimit, maxLimit]
     termList = []  # Lista de objetos termitas
     chipList = []  # Lista de objetos chips
     clist = list()  # Lista de objetos turtle para los chips
     tlist = list()  # Lista de objetos turtle para las termitas
+    stepsWalk = []  # Array que guarda los pasos recorridos
+    counter = 0  # Contador que se utilizara para saber el intervalo de pasos
+    win = False  # Variable para saber si ganamos
 
     """
     Crea una lista de objetos turtle y objetos termite
     """
     for pi in range(termites):
         termList.append(te.Termite(
-            (r.randint(limits[0], limits[1]), r.randint(limits[2], limits[3]))))
+            (r.randint(limits[0], limits[1]),
+             r.randint(limits[2], limits[3]))))  # Se genera la termite en un lugar aleatorio
         # Asigna forma circulo a cada turtle
         tlist.append(t.Turtle(shape="turtle"))
         tlist[pi].color(termList[pi].getColor())  # Asigna color rojo a turtle
@@ -47,31 +53,65 @@ def walk(steps, interval, termites, chips, maxLimit):
     #    tc.goto(termList[i].getPos())
 
     for ts in range(steps):
+
         # print(len(chipList), len(posChips))
         # print(posChips)
         for i, tc in enumerate(tlist):
-            # postions of all chips in the canvas
             posChips = {c.getPos(): c.index for c in chipList}
-
-            posT = termList[i].checkChip(chipList, posChips, r, limits, interval, termList[i], tc)
-
-            # Change color of chip if pickChip NOT returns None
-            if posT is not None:
-                # if ts > steps - 100:
-                ind = posChips[posT]
-                clist[ind].color(chipList[ind].getColor())
-                clist[ind].goto(chipList[ind].getPos())
-
-            termList[i].move(r, limits, interval)
-
-            # if ts > steps - 100:
+            # Validamos si la termite esta sobre una chip
+            if termList[i].posicion in posChips:
+                win = True
+                break
+            # Validamos si el contador es igual al intervalo de pasos en el que la chip debe cambiar su posicion
+            if counter == stepLimit:
+                counter = 0
+                posC = chipList[i].moveChip(chipList, posChips, limits, r)
+                # Actualizamos graficamente la posicion del chip
+                if posC is not None:
+                    ind = posChips[posC]
+                    clist[ind].color(chipList[ind].getColor())
+                    clist[ind].goto(chipList[ind].getPos())
+            counter += 1
+            # llamamos al metodo que controlara los procesos del termite
+            termList[i].checkMove(posChips, limits, interval, stepsWalk)
             tc.color(termList[i].getColor())
             tc.goto(termList[i].getPos())
-
-        #sleep(0.7)
+        # Si ganamos rompemos el ciclo de pasos
+        tc.hideturtle()
+        if win:
+            break
+        # sleep(0.5)
         # screen.update()
         screen.tracer(100)  # Se refrescara la pantalla cada 10 ejecuciones
+    t.penup()
+    t.colormode(255)
+    # Ubicamos el cursor en la esquina superior izquierda para mostrar el mensaje
+    t.goto(limits[0], limits[1] - 5)
+    # Escribimos si ganamos o no
+    if win:
+        t.write('GANASTE', move=False, font=('monaco', 20, 'bold'), align='left')
+    else:
+        t.write('NO GANASTE', move=False, font=('monaco', 20, 'bold'), align='left')
+    # Mostramos la lista de pasos que hemos recorrido
+    print('Lista de pasos: ', stepsWalk)
 
+    # GRAFICA LOS PASOS QUE DIO
+    # Ubicamos el cursor en la posicion del primer paso para empeza a graficar la ruta
+    t.goto(stepsWalk[0])
+    t.pendown()
+    # Por cada paso graficamos
+    for step in stepsWalk:
+        if win:
+            # punto verde
+            t.dot(8, 50, 205, 50)
+        else:
+            # punto amarillo
+            t.dot(8, 250, 205, 50)
+        t.goto(step)
+        sleep(0.1)
+        screen.tracer(1000000)
+    # dejamos de dibujar
+    t.penup()
     t.exitonclick()  # Al hacer clic sobre la ventana grafica se cerrara
 
 
@@ -86,16 +126,17 @@ def main(args):
     chips: number of chips
     canvas_limit: canvas x,y in (-canvas_limit, canvas_limit)
     Ejemplo:
-    python walkTermites.py 1000 1 10 10 10
+    python walkTermites.py 1000 1 10 10 10 10
     """
 
-    if len(args) == 5:
+    if len(args) == 6:
         steps = int(args[0])
         interval = int(args[1])
         termites = int(args[2])
         chips = int(args[3])
         canvas_limit = int(args[4])
-        walk(steps, interval, termites, chips, canvas_limit)
+        stepLimit = int(args[5])  # Nuevo argumento para saber cada cuantos pasos movemos aleatoriamente la chip
+        walk(steps, interval, termites, chips, canvas_limit, stepLimit)
     else:
         print(main.__doc__)
 
